89c142f39f8cc668d7444c72d79aa8d7edcda36a
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-05-19 15:00:45 +0900

    [add]記事一覧

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all()->sortByDesc('created_at');
+        return view('posts.index', compact('posts'));
+    }
 }
