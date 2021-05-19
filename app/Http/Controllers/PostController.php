72ae0c2a7d3c936fd19249fdc77b1a725047b819
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-05-19 18:15:08 +0900

    [add]新規投稿機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Category;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,5 +12,17 @@ class PostController extends Controller
     public function index(){
         $posts = Post::all()->sortByDesc('created_at');
         return view('posts.index', compact('posts'));
+    }
+
+    public function create(){
+        $categories = Category::all();
+        return view('posts.create', compact('categories'));
+    }
+
+    public function store(PostRequest $request, Post $post){
+        $post->fill($request['post']);
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return redirect()->route('posts.index');
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,8 +7,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+        'image',
+        'category_id'
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo('App\Category');
     }
 }
