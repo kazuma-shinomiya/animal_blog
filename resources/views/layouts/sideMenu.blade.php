774a6cecd0645edc84a272962b4231c7364666fb
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-05-27 15:10:07 +0900

    [add]ページネーション

--- resources/views/layouts/sideMenu.blade.php
+++ resources/views/layouts/sideMenu.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-3 sidebar">
   @foreach($categories as $category)
-    <nav class="navbar navbar-light bg-white border-bottom">
+    <nav class="navbar navbar-light bg-white border-bottom ">
       <a class="navbar-brand" href="{{ route('posts.index', ['category' => $category]) }}">{{ $category->name }}</a>
     </nav>
   @endforeach
